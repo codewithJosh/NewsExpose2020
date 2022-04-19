663c1f574c6df74674747808a3fee6e2d632c12d
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-20 01:29:32 +0800

    feat: load current `user_image` into `civ_user_image`

--- app/src/main/java/com/codewithjosh/NewsExpose2k20/CommentActivity.java
+++ app/src/main/java/com/codewithjosh/NewsExpose2k20/CommentActivity.java
@@ -6,12 +6,25 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.codewithjosh.NewsExpose2k20.models.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class CommentActivity extends AppCompatActivity {
 
     EditText add_comment;
     ImageView image_profile, send, back;
+
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +43,27 @@ public class CommentActivity extends AppCompatActivity {
             }
         });
 
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        getImage();
+
+    }
+
+    private void getImage(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                Glide.with(getApplicationContext()).load(user.getImageurl()).into(image_profile);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
