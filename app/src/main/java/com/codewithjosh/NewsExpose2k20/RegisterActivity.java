af7add2f6261a405b78c3d39a217f190b2a9943e
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-19 21:29:14 +0800

    feat: add `login` nav

--- app/src/main/java/com/codewithjosh/NewsExpose2k20/RegisterActivity.java
+++ app/src/main/java/com/codewithjosh/NewsExpose2k20/RegisterActivity.java
@@ -1,14 +1,39 @@
 package com.codewithjosh.NewsExpose2k20;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText usernameEditText, emailEditText, passwordEditText, repasswordEditText;
+    Button register;
+    TextView loginTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        usernameEditText = findViewById(R.id.usernameEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        repasswordEditText = findViewById(R.id.repasswordEditText);
+        register = findViewById(R.id.register);
+        loginTextView = findViewById(R.id.loginTextView);
+
+        loginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
     }
+
 }
