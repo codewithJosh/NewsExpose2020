98f6fba3396a6fa14d12818a4634b7f6e1cb8625
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-19 23:37:05 +0800

    feat: add `back` button

--- app/src/main/java/com/codewithjosh/NewsExpose2k20/CreateUpdateActivity.java
+++ app/src/main/java/com/codewithjosh/NewsExpose2k20/CreateUpdateActivity.java
@@ -1,14 +1,38 @@
 package com.codewithjosh.NewsExpose2k20;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateUpdateActivity extends AppCompatActivity {
 
+    ImageView close, image_added;
+    TextView update;
+    EditText subject;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_update);
+
+        close = findViewById(R.id.back);
+        image_added = findViewById(R.id.image_added);
+        update = findViewById(R.id.update);
+        subject = findViewById(R.id.subject);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CreateUpdateActivity.this, HomeActivity.class));
+                finish();
+            }
+        });
+
     }
+
 }
