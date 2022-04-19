188c961050a38389c6e60bee85e5fba225d8550f
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-19 21:38:52 +0800

    feat: add `register` button

--- app/src/main/java/com/codewithjosh/NewsExpose2k20/RegisterActivity.java
+++ app/src/main/java/com/codewithjosh/NewsExpose2k20/RegisterActivity.java
@@ -1,19 +1,39 @@
 package com.codewithjosh.NewsExpose2k20;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText usernameEditText, emailEditText, passwordEditText, repasswordEditText;
     Button register;
     TextView loginTextView;
+
+    FirebaseAuth auth;
+
+    DatabaseReference reference;
+
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +54,74 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+        auth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pd = new ProgressDialog(RegisterActivity.this);
+                pd.setMessage("Signing up");
+                pd.show();
+
+                String str_username = usernameEditText.getText().toString();
+                String str_email = emailEditText.getText().toString();
+                String str_password = passwordEditText.getText().toString();
+                String str_re_password = repasswordEditText.getText().toString();
+
+                if(TextUtils.isEmpty(str_username) || TextUtils.isEmpty(str_email)
+                        || TextUtils.isEmpty(str_password) || TextUtils.isEmpty(str_re_password)) {
+                    pd.dismiss();
+                    Toast.makeText(RegisterActivity.this, "All fields are required!", Toast.LENGTH_SHORT).show();
+                } else if (str_password.length() < 6) {
+                    pd.dismiss();
+                    Toast.makeText(RegisterActivity.this, "Password must atleast 6 characters", Toast.LENGTH_SHORT).show();
+                } else if (!(str_password.equals(str_re_password))) {
+                    pd.dismiss();
+                    Toast.makeText(RegisterActivity.this, "Password doesn't match!", Toast.LENGTH_SHORT).show();
+                } else {
+                    register(str_username, str_email, str_password);
+                }
+            }
+        });
+
+    }
+
+    private void register(final String username, String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            String userid = firebaseUser.getUid();
+
+                            reference = FirebaseDatabase.getInstance().getReference().child("Users").child(userid);
+
+                            HashMap<String, Object> hashMap = new HashMap<>();
+                            hashMap.put("id", userid);
+                            hashMap.put("username", username);
+                            hashMap.put("bio", "");
+                            hashMap.put("imageurl", "https://firebasestorage.googleapis.com/v0/b/news-expose-2k20.appspot.com/o/20220410_Res%2FDefaultUserImage.png?alt=media&token=20ff6824-a3b5-4cb2-8203-7ce9ef45a51a");
+                            hashMap.put("admin", false);
+
+                            reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        pd.dismiss();
+                                        Toast.makeText(RegisterActivity.this, "You're Successfully Added!", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                    }
+                                }
+                            });
+                        } else {
+                            pd.dismiss();
+                            Toast.makeText(RegisterActivity.this, "Incorrect Email Address", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
 }
