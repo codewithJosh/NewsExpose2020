2e1d9f297dcb22d27127f10a8f333ec783feba3c
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-19 22:31:52 +0800

    feat: add `home` and `profile` fragment nav

--- app/src/main/java/com/codewithjosh/NewsExpose2k20/HomeActivity.java
+++ app/src/main/java/com/codewithjosh/NewsExpose2k20/HomeActivity.java
@@ -1,12 +1,15 @@
 package com.codewithjosh.NewsExpose2k20;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.codewithjosh.NewsExpose2k20.fragments.HomeFragment;
+import com.codewithjosh.NewsExpose2k20.fragments.ProfileFragment;
 import com.codewithjosh.NewsExpose2k20.models.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -47,6 +50,25 @@ public class HomeActivity extends AppCompatActivity {
 
             }
         });
+
+        nav_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HomeFragment()).commit();
+            }
+        });
+
+        nav_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+            }
+        });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
 
     }
 
