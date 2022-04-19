fd36ff4ca41569ddc4a6082694600c679f9abda4
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-19 23:54:21 +0800

    feat: add `crop_image` activity

--- app/src/main/java/com/codewithjosh/NewsExpose2k20/CreateUpdateActivity.java
+++ app/src/main/java/com/codewithjosh/NewsExpose2k20/CreateUpdateActivity.java
@@ -1,19 +1,26 @@
 package com.codewithjosh.NewsExpose2k20;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.theartofdev.edmodo.cropper.CropImage;
 
 public class CreateUpdateActivity extends AppCompatActivity {
 
     ImageView close, image_added;
     TextView update;
     EditText subject;
+
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,26 @@ public class CreateUpdateActivity extends AppCompatActivity {
             }
         });
 
+        CropImage.activity()
+                .setAspectRatio(1,1)
+                .start(CreateUpdateActivity.this);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            imageUri = result.getUri();
+
+            image_added.setImageURI(imageUri);
+        } else {
+            Toast.makeText(this, "Something gone wrong!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(CreateUpdateActivity.this, HomeActivity.class));
+            finish();
+        }
     }
 
 }
