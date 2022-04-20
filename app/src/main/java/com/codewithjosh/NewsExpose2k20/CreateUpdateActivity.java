6eb1568df0cc8e61ed76340cbde56f3786f8dbdd
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-20 20:57:54 +0800

    refactor: update `create_update` activity

--- app/src/main/java/com/codewithjosh/NewsExpose2k20/CreateUpdateActivity.java
+++ app/src/main/java/com/codewithjosh/NewsExpose2k20/CreateUpdateActivity.java
@@ -1,78 +1,129 @@
 package com.codewithjosh.NewsExpose2k20;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 
 import java.util.HashMap;
 
 public class CreateUpdateActivity extends AppCompatActivity {
 
-    ImageView close, image_added;
-    TextView update;
-    EditText subject;
+    EditText et_update_content;
+    ImageButton btn_back;
+    ImageView iv_update_image;
+    TextView btn_create_update;
 
-    Uri imageUri;
-    String myUrl = "";
-    StorageTask uploadTask;
+    String s_update_image;
+    UploadTask uTask;
+    Uri uri;
 
-    StorageReference storageReference;
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+    FirebaseStorage firebaseStorage;
+
+    StorageReference storageRef;
+
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_update);
 
-        close = findViewById(R.id.btn_back);
-        image_added = findViewById(R.id.iv_update_image);
-        update = findViewById(R.id.btn_create_update);
-        subject = findViewById(R.id.et_update_content);
+        et_update_content = findViewById(R.id.et_update_content);
+        btn_back = findViewById(R.id.btn_back);
+        iv_update_image = findViewById(R.id.iv_update_image);
+        btn_create_update = findViewById(R.id.btn_create_update);
 
-        close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(CreateUpdateActivity.this, HomeActivity.class));
-                finish();
-            }
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+
+        storageRef = firebaseStorage.getReference("Updates");
+
+        btn_back.setOnClickListener(v -> {
+            startActivity(new Intent(this, HomeActivity.class));
+            finish();
         });
+
+        btn_create_update.setOnClickListener(v -> onUpdate());
 
         CropImage.activity()
-                .setAspectRatio(1, 1)
-                .start(CreateUpdateActivity.this);
+                .setAspectRatio(1,1)
+                .start(this);
+    }
 
-        storageReference = FirebaseStorage.getInstance().getReference("Updates");
+    //    TODO: FOUND ISSUE: BUG
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
 
-        update.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                uploadImage();
-            }
-        });
+    private void onUpdate() {
+        pd = new ProgressDialog(this);
+        pd.setMessage("Updating");
+        pd.show();
+
+        if (uri != null) {
+            final StorageReference _storageRef = storageRef.child(System.currentTimeMillis()
+                    + "." + getFileExtension(uri));
+
+            uTask = _storageRef.putFile(uri);
+            uTask.continueWithTask(task -> {
+                if(!task.isSuccessful()) throw task.getException();
+                return _storageRef.getDownloadUrl();
+
+            }).addOnSuccessListener(uri -> {
+
+                if (uri != null) s_update_image = uri.toString();
+
+                DatabaseReference databaseRef = firebaseDatabase.getReference("Updates");
+
+                final String s_user_id = firebaseAuth.getCurrentUser().getUid();
+                final String s_update_id = databaseRef.push().getKey();
+                final String s_update_content = et_update_content.getText().toString();
+
+//                    TODO: FOUND ISSUE: UPDATE THE DETAILS
+                HashMap<String, Object> update = new HashMap<>();
+                update.put("updateid", s_update_id);
+                update.put("updateimage", s_update_image);
+                update.put("subject", s_update_content);
+                update.put("source", s_user_id);
+
+                if (s_update_id != null)
+
+                    databaseRef
+                            .child(s_update_id)
+                            .setValue(update)
+                            .addOnSuccessListener(runnable -> {
+                                pd.dismiss();
+                                startActivity(new Intent(this, HomeActivity.class));
+                                finish();
+                            });
+
+            });
+        }
+        else Toast.makeText(this, "No Image Selected!", Toast.LENGTH_SHORT).show();
 
     }
 
@@ -80,83 +131,15 @@ public class CreateUpdateActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK){
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            imageUri = result.getUri();
-
-            image_added.setImageURI(imageUri);
-        } else {
+            uri = result.getUri();
+            iv_update_image.setImageURI(uri);
+        }
+        else {
             Toast.makeText(this, "Something gone wrong!", Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(CreateUpdateActivity.this, HomeActivity.class));
+            startActivity(new Intent(this, HomeActivity.class));
             finish();
         }
     }
-
-    private void uploadImage() {
-        final ProgressDialog pd = new ProgressDialog(this);
-        pd.setMessage("Updating");
-        pd.show();
-
-        if (imageUri != null) {
-            final StorageReference filereference = storageReference.child(System.currentTimeMillis()
-                    + "." + getFileExtension(imageUri));
-
-            uploadTask = filereference.putFile(imageUri);
-            uploadTask.continueWithTask(new Continuation() {
-                @Override
-                public Object then(@NonNull Task task) throws Exception {
-                    if (!task.isSuccessful()) {
-                        throw task.getException();
-                    }
-
-                    return filereference.getDownloadUrl();
-                }
-            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
-                @Override
-                public void onComplete(@NonNull Task<Uri> task) {
-                    if (task.isSuccessful()) {
-                        Uri downloadUri = task.getResult();
-                        myUrl = downloadUri.toString();
-
-                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Updates");
-
-                        FirebaseUser firebaseUser;
-
-                        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
-                        String updateid = reference.push().getKey();
-
-                        HashMap<String, Object> hashMap = new HashMap<>();
-                        hashMap.put("updateid", updateid);
-                        hashMap.put("updateimage", myUrl);
-                        hashMap.put("subject", subject.getText().toString());
-                        hashMap.put("source", firebaseUser.getUid());
-
-                        reference.child(updateid).setValue(hashMap);
-
-                        pd.dismiss();
-
-                        startActivity(new Intent(CreateUpdateActivity.this, HomeActivity.class));
-                        finish();
-                    } else {
-                        Toast.makeText(CreateUpdateActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(CreateUpdateActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            });
-        } else {
-            Toast.makeText(this, "No Image Selected!", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private String getFileExtension(Uri uri) {
-        ContentResolver contentResolver = getContentResolver();
-        MimeTypeMap mime = MimeTypeMap.getSingleton();
-        return mime.getExtensionFromMimeType(contentResolver.getType(uri));
-    }
-
 }
