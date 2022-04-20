b0c41eac6626badf102d8fcc00631104a0903182
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-20 20:58:08 +0800

    refactor: update `home` fragment layout

--- app/src/main/java/com/codewithjosh/NewsExpose2k20/fragments/HomeFragment.java
+++ app/src/main/java/com/codewithjosh/NewsExpose2k20/fragments/HomeFragment.java
@@ -34,7 +34,7 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView = view.findViewById(R.id.recycler_updates);
         recyclerView.setHasFixedSize(true); // false
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         linearLayoutManager.setReverseLayout(true);
