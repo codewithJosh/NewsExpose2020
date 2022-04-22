2cea692dcb84a91432006484dd7ee78f478346ba
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-22 12:40:40 +0800

    feat: add `login` support

--- app/src/main/java/com/codewithjosh/NewsExpose2k20/LoginActivity.java
+++ app/src/main/java/com/codewithjosh/NewsExpose2k20/LoginActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.codewithjosh.NewsExpose2k20.models.UserModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,13 +19,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class LoginActivity extends AppCompatActivity {
 
     Button btn_login;
     EditText et_email, et_password;
     TextView nav_register;
 
-    String s_email, s_password;
+    int i_version_code;
 
     FirebaseAuth firebaseAuth;
     FirebaseDatabase firebaseDatabase;
@@ -43,6 +48,8 @@ public class LoginActivity extends AppCompatActivity {
         et_password = findViewById(R.id.et_password);
         nav_register = findViewById(R.id.nav_register);
 
+        i_version_code = BuildConfig.VERSION_CODE;
+
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseDatabase = FirebaseDatabase.getInstance();
 
@@ -56,49 +63,89 @@ public class LoginActivity extends AppCompatActivity {
             pd.setMessage("Logging in");
             pd.show();
 
-            s_email = et_email.getText().toString();
-            s_password = et_password.getText().toString();
+            final String s_email = et_email.getText().toString();
+            final String s_password = et_password.getText().toString();
 
             if (s_email.isEmpty() || s_password.isEmpty()) {
                 pd.dismiss();
                 Toast.makeText(this, "All fields are required!", Toast.LENGTH_SHORT).show();
-            } else {
+            }
+            else {
 
-                firebaseAuth.signInWithEmailAndPassword(s_email, s_password)
+                firebaseAuth
+                        .signInWithEmailAndPassword(s_email, s_password)
                         .addOnSuccessListener(authResult -> {
 
-                            final String s_user_id = firebaseAuth.getCurrentUser().getUid();
+                            final String s_user_id = authResult.getUser().getUid();
 
                             databaseRef = firebaseDatabase
-                                    .getReference()
-                                    .child("Users")
+                                    .getReference("Users")
                                     .child(s_user_id);
 
-//                            TODO: USE GET METHOD ONCE IT IS AVAILABLE
                             databaseRef
-                                    .addValueEventListener(new ValueEventListener() {
+                                    .get()
+                                    .addOnSuccessListener(dataSnapshot -> {
 
-                                        @Override
-                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                        final UserModel getUser = dataSnapshot.getValue(UserModel.class);
+
+                                        final int i_user_version_code = getUser.getUser_version_code();
+
+                                        if (i_user_version_code == i_version_code) {
+
                                             pd.dismiss();
-                                            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                                            startActivity(new Intent(this, HomeActivity.class));
                                             finish();
+                                        }
+                                        else if (i_user_version_code > i_version_code) {
 
+                                            firebaseAuth.signOut();
+                                            Toast.makeText(this, "Your account is incompatible to this version!", Toast.LENGTH_LONG).show();
+                                            startActivity(new Intent(this, MainActivity.class));
+                                            finish();
+                                        }
+                                        else {
+
+                                            final UserModel setUser = onMigrate(getUser, i_user_version_code);
+
+                                            databaseRef
+                                                    .setValue(setUser)
+                                                    .addOnSuccessListener(runnable -> {
+
+                                                        pd.dismiss();
+                                                        startActivity(new Intent(this, HomeActivity.class));
+                                                        finish();
+                                                    });
                                         }
 
-                                        @Override
-                                        public void onCancelled(@NonNull DatabaseError error) {
-                                            pd.dismiss();
+                                    }).addOnFailureListener(e -> pd.dismiss());
 
-                                        }
-                                    });
 
                         }).addOnFailureListener(e -> {
-                    pd.dismiss();
-                    Toast.makeText(this, "Incorrect Email or Password", Toast.LENGTH_SHORT).show();
-                });
+
+                            pd.dismiss();
+                            Toast.makeText(this, "Incorrect Email or Password", Toast.LENGTH_SHORT).show();
+                        });
             }
         });
+
+    }
+
+    private UserModel onMigrate(final UserModel getUser, final int i_user_version_code) {
+
+        UserModel user = new UserModel();
+
+        if (i_user_version_code < 5) {
+
+            user = new UserModel(
+                    getUser.getUser_bio(),
+                    getUser.getUser_email(),
+                    getUser.getUser_id(),
+                    getUser.getUser_image(),
+                    getUser.isUser_is_admin(),
+                    getUser.getUser_name(),
+                    i_version_code
+            );
+        } return user;
 
     }
 
