1d2971377e782bf044dbbf11abadc4bbba0c4abe
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-26 21:26:42 +0800

    refactor: update `main` activity

--- app/src/main/java/com/codewithjosh/NewsExpose2k20/MainActivity.java
+++ app/src/main/java/com/codewithjosh/NewsExpose2k20/MainActivity.java
@@ -2,6 +2,7 @@ package com.codewithjosh.NewsExpose2k20;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -29,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     FirebaseFirestore firebaseFirestore;
     FirebaseUser firebaseUser;
 
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -49,40 +53,20 @@ public class MainActivity extends AppCompatActivity {
                         if (value != null) {
 
                             if (value.exists()) checkCurrentAuthState();
-                            else {
 
-                                final String s_version_name = BuildConfig.VERSION_NAME;
+                            else onUnsupported();
 
-                                is_loading.setVisibility(View.GONE);
-                                is_unsupported.setVisibility(View.VISIBLE);
-                                tv_version_name.setText(s_version_name);
-                            }
                         }
                     });
         } else Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_LONG).show();
 
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private boolean isConnected() {
 
-        nav_login = findViewById(R.id.nav_login);
-        nav_register = findViewById(R.id.nav_register);
-        is_loading = findViewById(R.id.is_loading);
-        is_unsupported = findViewById(R.id.is_unsupported);
-        tv_version_name = findViewById(R.id.tv_version_name);
-
-        nav_login.setOnClickListener(v -> {
-            startActivity(new Intent(this, LoginActivity.class));
-            finish();
-        });
-
-        nav_register.setOnClickListener(v -> {
-            startActivity(new Intent(this, RegisterActivity.class));
-            finish();
-        });
+        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
 
     }
 
@@ -105,30 +89,76 @@ public class MainActivity extends AppCompatActivity {
 
                         final UserModel user = documentSnapshot.toObject(UserModel.class);
 
-                        final String s_user_contact = user.getUser_contact();
-                        final boolean user_is_verified = user.isUser_is_verified();
+                        if (user != null) {
 
-                        if (user_is_verified) {
+                            final String s_user_contact = user.getUser_contact();
+                            final boolean user_is_verified = user.isUser_is_verified();
 
-                            startActivity(new Intent(this, HomeActivity.class));
-                        } else {
+                            if (user_is_verified) {
 
-                            Intent intent = new Intent(this, PhoneNumberActivity.class);
-                            intent.putExtra("s_user_id", s_user_id);
-                            intent.putExtra("s_user_contact", s_user_contact);
-                            startActivity(intent);
+                                startActivity(new Intent(this, HomeActivity.class));
+                            } else {
+
+                                editor.putString("s_user_contact", s_user_contact);
+                                editor.apply();
+                                startActivity(new Intent(this, PhoneNumberActivity.class));
+                            }
+                            finish();
                         }
-                        finish();
                     });
         }
 
     }
 
-    private boolean isConnected() {
+    private void onUnsupported() {
 
-        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-        return networkInfo != null && networkInfo.isConnectedOrConnecting();
+        final String s_version_name = BuildConfig.VERSION_NAME;
+
+        is_loading.setVisibility(View.GONE);
+        is_unsupported.setVisibility(View.VISIBLE);
+        tv_version_name.setText(s_version_name);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        initSharedPref();
+        buildNavButton();
+
+    }
+
+    private void initView() {
+
+        nav_login = findViewById(R.id.nav_login);
+        nav_register = findViewById(R.id.nav_register);
+        is_loading = findViewById(R.id.is_loading);
+        is_unsupported = findViewById(R.id.is_unsupported);
+        tv_version_name = findViewById(R.id.tv_version_name);
+
+    }
+
+    private void initSharedPref() {
+
+        sharedPref = getSharedPreferences("user", MODE_PRIVATE);
+        editor = sharedPref.edit();
+
+    }
+
+    private void buildNavButton() {
+
+        nav_login.setOnClickListener(v -> {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        });
+
+        nav_register.setOnClickListener(v -> {
+            startActivity(new Intent(this, RegisterActivity.class));
+            finish();
+        });
 
     }
 
