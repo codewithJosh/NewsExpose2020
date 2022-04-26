9de8655d6be07648c85c2beb71a99f68ca0f7e3b
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-26 14:30:05 +0800

    feat: add new `UserModel` model

--- app/src/main/java/com/codewithjosh/NewsExpose2k20/models/UserModel.java
+++ app/src/main/java/com/codewithjosh/NewsExpose2k20/models/UserModel.java
@@ -3,10 +3,12 @@ package com.codewithjosh.NewsExpose2k20.models;
 public class UserModel {
 
     private String user_bio;
+    private String user_contact;
     private String user_email;
     private String user_id;
     private String user_image;
     private boolean user_is_admin;
+    private boolean user_is_verified;
     private String user_name;
     private int user_version_code;
 
@@ -23,60 +25,52 @@ public class UserModel {
         this.user_version_code = user_version_code;
     }
 
+    public UserModel(String user_bio, String user_contact, String user_email, String user_id, String user_image, boolean user_is_admin, boolean user_is_verified, String user_name, int user_version_code) {
+        this.user_bio = user_bio;
+        this.user_contact = user_contact;
+        this.user_email = user_email;
+        this.user_id = user_id;
+        this.user_image = user_image;
+        this.user_is_admin = user_is_admin;
+        this.user_is_verified = user_is_verified;
+        this.user_name = user_name;
+        this.user_version_code = user_version_code;
+    }
+
     public String getUser_bio() {
         return user_bio;
     }
 
-    public void setUser_bio(String user_bio) {
-        this.user_bio = user_bio;
+    public String getUser_contact() {
+        return user_contact;
     }
 
     public String getUser_email() {
         return user_email;
     }
 
-    public void setUser_email(String user_email) {
-        this.user_email = user_email;
-    }
-
     public String getUser_id() {
         return user_id;
-    }
-
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
     }
 
     public String getUser_image() {
         return user_image;
     }
 
-    public void setUser_image(String user_image) {
-        this.user_image = user_image;
-    }
-
     public boolean isUser_is_admin() {
         return user_is_admin;
     }
 
-    public void setUser_is_admin(boolean user_is_admin) {
-        this.user_is_admin = user_is_admin;
+    public boolean isUser_is_verified() {
+        return user_is_verified;
     }
 
     public String getUser_name() {
         return user_name;
     }
 
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
-    }
-
     public int getUser_version_code() {
         return user_version_code;
-    }
-
-    public void setUser_version_code(int user_version_code) {
-        this.user_version_code = user_version_code;
     }
 
 }
