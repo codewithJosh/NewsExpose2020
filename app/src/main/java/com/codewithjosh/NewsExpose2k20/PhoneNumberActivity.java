6b214182871278d5d1acd0a9ee132825350b8f0d
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-27 22:14:05 +0800

    refactor: update `phone_number` activity

--- app/src/main/java/com/codewithjosh/NewsExpose2k20/PhoneNumberActivity.java
+++ app/src/main/java/com/codewithjosh/NewsExpose2k20/PhoneNumberActivity.java
@@ -2,6 +2,7 @@ package com.codewithjosh.NewsExpose2k20;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -22,10 +23,15 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.codewithjosh.NewsExpose2k20.models.UserModel;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.safetynet.SafetyNet;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.auth.User;
 import com.hbb20.CountryCodePicker;
 
 import org.json.JSONObject;
@@ -44,15 +50,30 @@ public class PhoneNumberActivity extends AppCompatActivity {
     EditText et_contact;
     LinearLayout is_loading;
     TextView btn_skip;
-    String s_user_id, s_user_contact;
+    String s_user_id, s_user_contact, s_contact;
     FirebaseFirestore firebaseFirestore;
 
     RequestQueue req;
+
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
+
+    DocumentReference documentRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_number);
+
+        initViews();
+        initInstance();
+        initSharedPref();
+        load();
+        build();
+
+    }
+
+    private void initViews() {
 
         btn_next = findViewById(R.id.btn_next);
         cb_recaptcha = findViewById(R.id.cb_recaptcha);
@@ -61,30 +82,47 @@ public class PhoneNumberActivity extends AppCompatActivity {
         is_loading = findViewById(R.id.is_loading);
         btn_skip = findViewById(R.id.btn_skip);
 
-        s_user_id = getIntent().getStringExtra("s_user_id");
-        s_user_contact = getIntent().getStringExtra("s_user_contact");
+    }
+
+    private void initInstance() {
 
         firebaseFirestore = FirebaseFirestore.getInstance();
+        req = Volley.newRequestQueue(getApplicationContext());
 
+    }
+
+    private void initSharedPref() {
+
+        sharedPref = getSharedPreferences("user", MODE_PRIVATE);
+        editor = sharedPref.edit();
+
+    }
+
+    private void load() {
+
+        s_user_id = sharedPref.getString("s_user_id", String.valueOf(MODE_PRIVATE));
+        s_user_contact = sharedPref.getString("s_user_contact", String.valueOf(MODE_PRIVATE));
         et_contact.setText(s_user_contact);
+
+    }
+
+    private void build() {
 
         btn_skip.setOnClickListener(v -> {
 
             is_loading.setVisibility(View.VISIBLE);
 
-            if (isConnected())
+            if (isConnected()) {
 
-                firebaseFirestore
-                        .collection("Users")
-                        .document(s_user_id)
-                        .update("user_contact", "", "user_is_verified", true)
-                        .addOnSuccessListener(unused -> {
+                final String s_user_contact = "";
+                final boolean user_is_verified = true;
 
-                            is_loading.setVisibility(View.GONE);
-                            Toast.makeText(this, "Welcome, You've Successfully Login!", Toast.LENGTH_LONG).show();
-                            startActivity(new Intent(this, HomeActivity.class));
-                            finish();
-                        });
+                final HashMap<String, Object> user = new HashMap<>();
+                user.put("user_contact", s_user_contact);
+                user.put("user_is_verified", user_is_verified);
+
+                updateUser(user);
+            }
             else {
                 is_loading.setVisibility(View.GONE);
                 Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_SHORT).show();
@@ -92,64 +130,17 @@ public class PhoneNumberActivity extends AppCompatActivity {
 
         });
 
-        ccp_country.registerCarrierNumberEditText(et_contact);
-
         btn_next.setOnClickListener(v -> {
 
-            is_loading.setVisibility(View.VISIBLE);
-            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(CommentActivity.INPUT_METHOD_SERVICE);
-            inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
-            if (getCurrentFocus() != null) getCurrentFocus().clearFocus();
+            s_contact = et_contact.getText().toString();
 
-            String s_contact = et_contact.getText().toString();
+            if (validate(v)) checkContact();
 
-            if (!isConnected()) {
-                is_loading.setVisibility(View.GONE);
-                Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_SHORT).show();
-            } else if (s_contact.isEmpty()) {
-                is_loading.setVisibility(View.GONE);
-                Toast.makeText(this, "Phone Number is required!", Toast.LENGTH_SHORT).show();
-            } else if (!s_contact.startsWith("09")) {
-                is_loading.setVisibility(View.GONE);
-                Toast.makeText(this, "Provide a valid Phone Number", Toast.LENGTH_SHORT).show();
-            } else if (s_contact.length() < 11) {
-                is_loading.setVisibility(View.GONE);
-                Toast.makeText(this, "Phone Number must be at least 11 digits", Toast.LENGTH_SHORT).show();
-            } else if (!cb_recaptcha.isChecked()) {
-                is_loading.setVisibility(View.GONE);
-                onRecaptcha();
-            } else {
+            else is_loading.setVisibility(View.GONE);
 
-                firebaseFirestore
-                        .collection("Users")
-                        .whereEqualTo("user_contact", s_contact)
-                        .get()
-                        .addOnSuccessListener(queryDocumentSnapshots -> {
-
-                            if (queryDocumentSnapshots.isEmpty()) {
-
-                                firebaseFirestore
-                                        .collection("Users")
-                                        .document(s_user_id)
-                                        .update("user_contact", s_contact)
-                                        .addOnSuccessListener(runnable -> {
-
-                                            is_loading.setVisibility(View.GONE);
-
-                                            Intent intent = new Intent(this, VerificationActivity.class);
-                                            intent.putExtra("s_user_contact", ccp_country.getFullNumberWithPlus());
-                                            startActivity(intent);
-                                            finish();
-                                        });
-                            } else {
-                                is_loading.setVisibility(View.GONE);
-                                Toast.makeText(this, "Phone Number is Unavailable!", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-            }
         });
 
-        req = Volley.newRequestQueue(getApplicationContext());
+        ccp_country.registerCarrierNumberEditText(et_contact);
 
         cb_recaptcha.setOnClickListener(v -> onRecaptcha());
 
@@ -160,6 +151,55 @@ public class PhoneNumberActivity extends AppCompatActivity {
         ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         return networkInfo != null && networkInfo.isConnectedOrConnecting();
+
+    }
+
+    private void updateUser(final HashMap<String, Object> user) {
+
+        documentRef = firebaseFirestore
+                .collection("Users")
+                .document(s_user_id);
+
+        documentRef
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+
+                    if (documentSnapshot != null)
+
+                        if (documentSnapshot.exists())
+
+                            documentRef
+                                    .update(user)
+                                    .addOnSuccessListener(unused -> {
+
+                                        Toast.makeText(this, "Welcome, You've Successfully Login!", Toast.LENGTH_LONG).show();
+                                        startActivity(new Intent(this, HomeActivity.class));
+                                        finish();
+                                    });
+                });
+
+    }
+
+    private boolean validate(final View v) {
+
+        is_loading.setVisibility(View.VISIBLE);
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(CommentActivity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+        if (getCurrentFocus() != null) getCurrentFocus().clearFocus();
+
+        if (!isConnected()) Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_SHORT).show();
+
+        else if (s_contact.isEmpty()) Toast.makeText(this, "Phone Number is required!", Toast.LENGTH_SHORT).show();
+
+        else if (!s_contact.startsWith("09")) Toast.makeText(this, "Provide a valid Phone Number", Toast.LENGTH_SHORT).show();
+
+        else if (s_contact.length() < 11) Toast.makeText(this, "Phone Number must be at least 11 digits", Toast.LENGTH_SHORT).show();
+
+        else if (!cb_recaptcha.isChecked()) onRecaptcha();
+
+        else return true;
+
+        return false;
 
     }
 
@@ -234,6 +274,54 @@ public class PhoneNumberActivity extends AppCompatActivity {
         ));
 
         req.add(sr_req);
+
+    }
+
+    private void checkContact() {
+
+        firebaseFirestore
+                .collection("Users")
+                .whereEqualTo("user_contact", s_contact)
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+
+                    final HashMap<String, Object> user = new HashMap<>();
+                    user.put("user_contact", s_contact);
+
+                    if (queryDocumentSnapshots != null)
+
+                        if (!queryDocumentSnapshots.isEmpty())
+
+                            for (QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+
+                                final UserModel getUser = snapshot.toObject(UserModel.class);
+                                final String _s_user_id = getUser.getUser_id();
+
+                                if (!_s_user_id.equals(s_user_id)) {
+
+                                    is_loading.setVisibility(View.GONE);
+                                    Toast.makeText(this, "Phone Number is Unavailable!", Toast.LENGTH_SHORT).show();
+                                } else onNext(user);
+                            }
+                        else onNext(user);
+
+                });
+
+    }
+
+    private void onNext(final HashMap<String, Object> user) {
+
+        firebaseFirestore
+                .collection("Users")
+                .document(s_user_id)
+                .update(user)
+                .addOnSuccessListener(runnable -> {
+
+                    editor.putString("s_user_contact", ccp_country.getFullNumberWithPlus());
+                    editor.apply();
+                    startActivity(new Intent(this, VerificationActivity.class));
+                    finish();
+                });
 
     }
 
