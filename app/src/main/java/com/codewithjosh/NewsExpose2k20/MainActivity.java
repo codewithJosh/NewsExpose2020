dca8d23acbcee8af1e801f9058ece00a2a6efc3b
codewithJosh <dacasinjoshua21@gmail.com>
2022-07-13 23:37:28 +0800

    refactor: update `Main` activity

--- app/src/main/java/com/codewithjosh/NewsExpose2k20/MainActivity.java
+++ app/src/main/java/com/codewithjosh/NewsExpose2k20/MainActivity.java
@@ -21,32 +21,33 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button nav_login, nav_register;
-    ConstraintLayout is_loading, is_unsupported;
-    TextView tv_version_name;
-
+    Button navLogin;
+    Button navRegister;
+    ConstraintLayout isLoading;
+    ConstraintLayout isUnsupported;
+    TextView tvVersionName;
     FirebaseFirestore firebaseFirestore;
     FirebaseUser firebaseUser;
-
-    SharedPreferences sharedPref;
     SharedPreferences.Editor editor;
 
     @Override
     protected void onStart() {
+
         super.onStart();
 
-        is_loading.setVisibility(View.VISIBLE);
+        isLoading.setVisibility(View.VISIBLE);
 
         firebaseFirestore = FirebaseFirestore.getInstance();
 
-        final String s_version_code = String.valueOf(BuildConfig.VERSION_CODE);
+        final String versionCode = String.valueOf(BuildConfig.VERSION_CODE);
 
-        if (isConnected()) {
+        if (isConnected())
 
             firebaseFirestore
                     .collection("Supports")
-                    .document(s_version_code)
-                    .addSnapshotListener((value, error) -> {
+                    .document(versionCode)
+                    .addSnapshotListener((value, error) ->
+                    {
 
                         if (value != null)
 
@@ -55,108 +56,123 @@ public class MainActivity extends AppCompatActivity {
                             else onUnsupported();
 
                     });
-        } else Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_LONG).show();
+
+        else Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_LONG).show();
 
     }
 
     private boolean isConnected() {
 
-        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        final ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        final NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         return networkInfo != null && networkInfo.isConnectedOrConnecting();
 
     }
 
     private void checkCurrentAuthState() {
 
-        is_loading.setVisibility(View.GONE);
-        is_unsupported.setVisibility(View.GONE);
+        isLoading.setVisibility(View.GONE);
+        isUnsupported.setVisibility(View.GONE);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
-        if (firebaseUser != null) {
+        if (firebaseUser != null)
+        {
 
-            final String s_user_id = firebaseUser.getUid();
+            final String userId = firebaseUser.getUid();
 
-            editor.putString("s_user_id", s_user_id);
+            editor.putString("user_id", userId);
             editor.apply();
 
             firebaseFirestore
                     .collection("Users")
-                    .document(s_user_id)
+                    .document(userId)
                     .get()
-                    .addOnSuccessListener(documentSnapshot -> {
+                    .addOnSuccessListener(documentSnapshot ->
+                    {
 
                         final UserModel user = documentSnapshot.toObject(UserModel.class);
 
-                        if (user != null) {
+                        if (user != null)
+                        {
 
-                            final String s_user_contact = user.getUser_contact();
-                            final boolean user_is_verified = user.isUser_is_verified();
+                            final String userContact = user.getUser_contact();
+                            final boolean userIsVerified = user.isUser_is_verified();
 
-                            if (user_is_verified)
-                                startActivity(new Intent(this, HomeActivity.class));
+                            if (userIsVerified) startActivity(new Intent(this, HomeActivity.class));
+
                             else {
 
-                                editor.putString("s_user_contact", s_user_contact);
+                                editor.putString("user_contact", userContact);
                                 editor.apply();
                                 startActivity(new Intent(this, PhoneNumberActivity.class));
+
                             }
+
                             finish();
+
                         }
+
                     });
+
         }
 
     }
 
     private void onUnsupported() {
 
-        final String s_version_name = BuildConfig.VERSION_NAME;
+        final String versionName = BuildConfig.VERSION_NAME;
 
-        is_loading.setVisibility(View.GONE);
-        is_unsupported.setVisibility(View.VISIBLE);
-        tv_version_name.setText(s_version_name);
+        isLoading.setVisibility(View.GONE);
+        isUnsupported.setVisibility(View.VISIBLE);
+        tvVersionName.setText(versionName);
 
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initView();
+        initViews();
         initSharedPref();
-        buildNavButton();
+        buildButtons();
 
     }
 
-    private void initView() {
+    private void initViews() {
 
-        nav_login = findViewById(R.id.nav_login);
-        nav_register = findViewById(R.id.nav_register);
-        is_loading = findViewById(R.id.is_loading);
-        is_unsupported = findViewById(R.id.is_unsupported);
-        tv_version_name = findViewById(R.id.tv_version_name);
+        navLogin = findViewById(R.id.nav_login);
+        navRegister = findViewById(R.id.nav_register);
+        isLoading = findViewById(R.id.is_loading);
+        isUnsupported = findViewById(R.id.is_unsupported);
+        tvVersionName = findViewById(R.id.tv_version_name);
 
     }
 
     private void initSharedPref() {
 
-        sharedPref = getSharedPreferences("user", MODE_PRIVATE);
-        editor = sharedPref.edit();
+        editor = getSharedPreferences("user", MODE_PRIVATE).edit();
 
     }
 
-    private void buildNavButton() {
+    private void buildButtons() {
 
-        nav_login.setOnClickListener(v -> {
+        navLogin.setOnClickListener(v ->
+        {
+
             startActivity(new Intent(this, LoginActivity.class));
             finish();
+
         });
 
-        nav_register.setOnClickListener(v -> {
+        navRegister.setOnClickListener(v ->
+        {
+
             startActivity(new Intent(this, RegisterActivity.class));
             finish();
+
         });
 
     }
