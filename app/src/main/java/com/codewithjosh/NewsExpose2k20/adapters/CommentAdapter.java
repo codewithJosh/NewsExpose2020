60c1b21594f80956eda9b8eb5b6dac6457c9c95e
codewithJosh <dacasinjoshua21@gmail.com>
2022-07-14 10:04:33 +0800

    refactor: update load `comments` into `item_comment`

--- app/src/main/java/com/codewithjosh/NewsExpose2k20/adapters/CommentAdapter.java
+++ app/src/main/java/com/codewithjosh/NewsExpose2k20/adapters/CommentAdapter.java
@@ -15,6 +15,8 @@ import com.codewithjosh.NewsExpose2k20.models.CommentModel;
 import com.codewithjosh.NewsExpose2k20.models.UserModel;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -23,119 +25,157 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ViewHolder> {
 
-    private static final int SECOND_MILLIS = 1000;
-    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
-    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
-    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
-
+    private static final int secondMillis = 1000;
+    private static final int minuteMillis = 60 * secondMillis;
+    private static final int hourMillis = 60 * minuteMillis;
+    private static final int dayMillis = 24 * hourMillis;
+    private static final int weekMillis = 7 * dayMillis;
     public Context context;
-    public List<CommentModel> commentList;
+    public List<CommentModel> comments;
+    DateFormat dateFormat;
     FirebaseFirestore firebaseFirestore;
 
-    public CommentAdapter(Context context, List<CommentModel> commentList) {
+    public CommentAdapter(final Context context, final List<CommentModel> comments) {
+
         this.context = context;
-        this.commentList = commentList;
+        this.comments = comments;
+
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+
         View view = LayoutInflater.from(context).inflate(R.layout.item_comment, viewGroup, false);
         return new ViewHolder(view);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-        final CommentModel comment = commentList.get(position);
+        final CommentModel comment = comments.get(position);
 
 //        initViews
-        final CircleImageView civ_user_image = holder.civ_user_image;
-        final TextView tv_comment_content = holder.tv_comment_content;
-        final TextView tv_subtitle = holder.tv_subtitle;
-        final TextView tv_user_bio = holder.tv_user_bio;
+        final CircleImageView civUserImage = holder.civUserImage;
+        final TextView tvCommentContent = holder.tvCommentContent;
+        final TextView tvSubtitle = holder.tvSubtitle;
+        final TextView tvUserBio = holder.tvUserBio;
 
 //        load
-        final String s_comment_content = comment.getComment_content();
-        final Date date_comment_timestamp = comment.getComment_timestamp();
-        final String s_user_id = comment.getUser_id();
+        final String commentContent = comment.getComment_content();
+        final Date commentTimestamp = comment.getComment_timestamp();
+        final String userId = comment.getUser_id();
 
-        firebaseFirestore = FirebaseFirestore.getInstance();
+        initInstances();
 
-        loadUser(civ_user_image, tv_user_bio, tv_subtitle, date_comment_timestamp, s_user_id);
+        loadUser(civUserImage, tvUserBio, tvSubtitle, commentTimestamp, userId);
 
-        tv_comment_content.setText(s_comment_content);
+        tvCommentContent.setText(commentContent);
 
     }
 
-    private void loadUser(final CircleImageView civ_user_image, final TextView tv_user_bio, TextView tv_subtitle, Date date_comment_timestamp, final String s_user_id) {
+    private void initInstances()
+    {
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+    }
+
+    private void loadUser(final CircleImageView civUserImage, final TextView tvUserBio, TextView tvSubtitle, Date commentTimestamp, final String userId) {
 
         firebaseFirestore
                 .collection("Users")
-                .document(s_user_id)
-                .addSnapshotListener((value, error) -> {
+                .document(userId)
+                .addSnapshotListener((value, error) ->
+                {
 
-                    if (value != null)
+                    if (value != null && value.exists())
+                    {
 
-                        if (value.exists()) {
+                        final UserModel user = value.toObject(UserModel.class);
 
-                            final UserModel user = value.toObject(UserModel.class);
+                        if (user != null)
+                        {
 
-                            if (user != null) {
+                            final String userImage = user.getUser_image();
+                            final String userBio = user.getUser_bio();
+                            final String userName = user.getUser_name();
+                            final String subtitle = getTimeAgo(commentTimestamp) + " · " + userName;
 
-                                final String s_user_image = user.getUser_image();
-                                final String s_user_bio = user.getUser_bio();
-                                final String s_user_name = user.getUser_name();
-                                final String s_subtitle = getTimeAgo(date_comment_timestamp) + " · " + s_user_name;
+                            Glide.with(context).load(userImage).into(civUserImage);
+                            tvUserBio.setText(userBio);
+                            tvSubtitle.setText(subtitle);
 
-                                Glide.with(context).load(s_user_image).into(civ_user_image);
-                                tv_user_bio.setText(s_user_bio);
-                                tv_subtitle.setText(s_subtitle);
-                            }
                         }
+
+                    }
+
                 });
 
     }
 
-    private String getTimeAgo(final Date date_comment_timestamp) {
+    private String getTimeAgo(final Date commentTimestamp) {
 
         final Calendar calendar = Calendar.getInstance();
+        final String yearFormat = "yyyy";
+        final String commentTimestampWithinTheYearFormat = "MMMM d";
+        final String commentTimestampFormat = "MMMM d, yyyy";
 
-        long now = calendar.getTime().getTime();
-        long time = date_comment_timestamp.getTime();
+        dateFormat = new SimpleDateFormat(yearFormat);
+        final int yearNow = calendar.get(Calendar.YEAR);
+        final int year = Integer.parseInt(dateFormat.format(commentTimestamp));
+
+        dateFormat = new SimpleDateFormat(commentTimestampWithinTheYearFormat);
+        final String commentTimestampWithinTheYear = dateFormat.format(commentTimestamp);
+
+        dateFormat = new SimpleDateFormat(commentTimestampFormat);
+        final String _commentTimestamp = dateFormat.format(commentTimestamp);
+
+        final long now = calendar.getTime().getTime();
+        long time = commentTimestamp.getTime();
 
         if (time < 1000000000000L) time *= 1000;
         final long diff = now - time;
 
-        if (diff < MINUTE_MILLIS) return "Just now";
+        if (diff < minuteMillis) return "Just now";
 
-        else if (diff < 60 * MINUTE_MILLIS) return diff / MINUTE_MILLIS + "m";
+        else if (diff < 60 * minuteMillis) return diff / minuteMillis + "m";
 
-        else if (diff < 24 * HOUR_MILLIS) return diff / HOUR_MILLIS + "h";
+        else if (diff < 24 * hourMillis) return diff / hourMillis + "h";
 
-        else if (diff < 48 * HOUR_MILLIS) return "yesterday";
+        else if (diff < 7 * dayMillis) return diff / dayMillis + "d";
 
-        else return diff / DAY_MILLIS + "d";
+        else if (diff < 4L * weekMillis) return diff / weekMillis + "w";
+
+        else if (year == yearNow) return commentTimestampWithinTheYear;
+
+        else return _commentTimestamp;
 
     }
 
     @Override
     public int getItemCount() {
-        return commentList.size();
+
+        return comments.size();
+
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        CircleImageView civ_user_image;
-        TextView tv_user_bio, tv_subtitle, tv_comment_content;
+        public CircleImageView civUserImage;
+        public TextView tvUserBio;
+        public TextView tvSubtitle;
+        public TextView tvCommentContent;
 
         public ViewHolder(@NonNull View itemView) {
+
             super(itemView);
 
-            civ_user_image = itemView.findViewById(R.id.civ_user_image);
-            tv_user_bio = itemView.findViewById(R.id.tv_user_bio);
-            tv_subtitle = itemView.findViewById(R.id.tv_subtitle);
-            tv_comment_content = itemView.findViewById(R.id.tv_comment_content);
+            civUserImage = itemView.findViewById(R.id.civ_user_image);
+            tvUserBio = itemView.findViewById(R.id.tv_user_bio);
+            tvSubtitle = itemView.findViewById(R.id.tv_subtitle);
+            tvCommentContent = itemView.findViewById(R.id.tv_comment_content);
 
         }
 
